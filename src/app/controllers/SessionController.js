de7c74b02d880c1c844275442561f4fb62f385e9
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-02-14 20:29:27 -0300

    session generate token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+
 class SessionController {
-  store(req, res) {}
+  store(req, res) {
+    const { email, password } = req.body;
+
+    if (email === 'user@test.com' && password === '123456') {
+      const token = jwt.sign(
+        {
+          email,
+          password,
+        },
+        authConfig.secret,
+        {
+          expiresIn: authConfig.ttl,
+        },
+      );
+
+      return res.status(200).send({ token });
+    }
+
+    return res.status(404).send({ message: 'user not found' });
+  }
 }
 
 export default new SessionController();

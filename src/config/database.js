36686ccd93fd6001ea3c31fecc456202d85e5118
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-02-19 22:48:40 -0300

    refactor: config database

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  uri:
-    process.env.DB_URL ||
-    'mongodb://localhost:27017/customers_favorites_products',
+  uri: process.env.DB_URL || 'mongodb://localhost:27017/customers',
 };
